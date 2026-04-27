11756d281fcab2beeb239b76bcaa4f00368b036a
Henrique <h.cochinski@gmail.com>
2026-04-27 16:05:16 -0300

    1

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,12 @@
-print("Olá GitHub")
+def calcular_media(nota1, nota2, nota3):
+    soma = nota1 + nota2 +nota3
+    media = soma/3
+    return media
+#programa principal (fora da função)
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+n3 = float(input("Digite a terceira nota: "))
+
+resultado = calcular_media(n1,n2,n3)
+
+print(f"Média final: {resultado}")
