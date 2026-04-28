73b776cd45799df9bee3d82ce01dde1e78b5a5c3
Henrique <h.cochinski@gmail.com>
2026-04-28 16:37:45 -0300

    f

--- lacos.py
+++ lacos.py
@@ -1,11 +1,26 @@
-def criar_perfil(nome,idade,cidade):
-     print(f"{nome},{idade} anos, {cidade}")
+# def criar_perfil(nome,idade,cidade):
+#      print(f"{nome},{idade} anos, {cidade}")
 
-criar_perfil(cidade="Curitiba", nome="Julia", idade=25)
-#Julia,25 anos, Curitiba - funciona independente da ordem!
+# criar_perfil(cidade="Curitiba", nome="Julia", idade=25)
+# #Julia,25 anos, Curitiba - funciona independente da ordem!
 
-def somar_tudo(*numeros):
-    return sum(numeros)
-print(somar_tudo(1,2))
-print(somar_tudo(1,2,3,4))
-print(somar_tudo(10,20,30,40))
+# def somar_tudo(*numeros):
+#     return sum(numeros)
+# print(somar_tudo(1,2))
+# print(somar_tudo(1,2,3,4))
+# print(somar_tudo(10,20,30,40))
+
+#Criando um dicionario
+
+produto = {"nome": "teclado" , "preço": 150, "em estoque": True}
+
+#Acessando um valor
+print(produto["nome"])
+
+#Modificando um valor
+produto["preco"] = 135.00
+
+#Adicionando um novo par
+produto["quantidade"] = 10
+
+print(produto)
