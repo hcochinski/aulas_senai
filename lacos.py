05e88ca5c64ed65a7e5adcf733cac17991570e30
Henrique <h.cochinski@gmail.com>
2026-04-28 16:02:04 -0300

    saa

--- lacos.py
+++ lacos.py
@@ -6,6 +6,6 @@ criar_perfil(cidade="Curitiba", nome="Julia", idade=25)
 
 def somar_tudo(*numeros):
     return sum(numeros)
-somar_tudo(1,2)
-somar_tudo(1,2,3,4)
-somar_tudo(10,20,30,40)
+print(somar_tudo(1,2))
+print(somar_tudo(1,2,3,4))
+print(somar_tudo(10,20,30,40))
